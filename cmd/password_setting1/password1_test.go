8e468caef1a5c794f4db7a5aa5d5cef813b16d9d
hifeia <weaeghvs1@outlook.com>
2021-02-23 12:47:51 -0700

    new repository

---
+++ cmd/password_setting1/password1_test.go
@@ -0,0 +1,76 @@
+package aljazeera
+
+import (
+	"fmt"
+	"log"
+	"net/url"
+	"testing"
+	"time"
+
+	"github.com/hi20160616/fetcher/internal/htmldoc"
+)
+
+var p = PostFactory("https://chinese.aljazeera.net/news/2021/1/20/重返核协议并支持两国方案布林肯概述拜登政府的")
+
+func PostFactory(rawurl string) *Post {
+	url, err := url.Parse(rawurl)
+	if err != nil {
+		log.Printf("url parse err: %s", err)
+	}
+	return &Post{
+		Domain: url.Hostname(),
+		URL:    url,
+	}
+}
+
+func TestSetDate(t *testing.T) {
+	raw, doc, err := htmldoc.GetRawAndDoc(p.URL, 1*time.Minute)
+	if err != nil {
+		t.Errorf("GetRawAndDoc err: %v", err)
+	}
+	p.Raw, p.DOC = raw, doc
+	if err := setDate(p); err != nil {
+		t.Errorf("test setPost err: %v", doc)
+	}
+	want := "2021-01-20T01:55:53+00:00"
+	if p.Date != want {
+		t.Errorf("got: %v, want: %v", p.Date, want)
+	}
+}
+
+func TestSetTitle(t *testing.T) {
+	raw, doc, err := htmldoc.GetRawAndDoc(p.URL, 1*time.Minute)
+	if err != nil {
+		t.Errorf("GetRawAndDoc err: %v", err)
+	}
+	p.Raw, p.DOC = raw, doc
+	if err := setTitle(p); err != nil {
+		t.Errorf("test setPost err: %v", doc)
+	}
+	want := "重返核协议并支持两国方案：布林肯概述拜登政府的外交政策特征"
+	if p.Title != want {
+		t.Errorf("got: %v, want: %v", p.Title, want)
+	}
+}
+func TestSetPost(t *testing.T) {
+	raw, doc, err := htmldoc.GetRawAndDoc(p.URL, 1*time.Minute)
+	if err != nil {
+		t.Errorf("GetRawAndDoc err: %v", err)
+	}
+	p.Raw, p.DOC = raw, doc
+	if err := SetPost(p); err != nil {
+		t.Errorf("test setPost err: %v", doc)
+	}
+	fmt.Println(p.Title)
+	fmt.Println(p.Body)
+}
+
+func TestAljazeera(t *testing.T) {
+	raw, doc, err := htmldoc.GetRawAndDoc(p.URL, 1*time.Minute)
+	if err != nil {
+		t.Errorf("GetRawAndDoc err: %v", err)
+	}
+	p.Raw, p.DOC = raw, doc
+	tc, err := aljazeera(p)
+	fmt.Println(tc)
+}
---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"log"
+	"time"
+
+	"github.com/hi20160616/fetcher/internal/fetcher"
+)
+
+func main() {
+	worklist := []string{
+		"https://www.cna.com.tw/list/aopl.aspx",           // 国际
+		"https://news.ltn.com.tw/list/breakingnews/world", // 国际
+		"https://www.zaobao.com/realtime/world",
+		"https://www.zaobao.com/news/world",
+		"https://www.zaobao.com/realtime/china",
+		"https://www.zaobao.com/news/china",
+		"https://www.dwnews.com",
+		"https://www.dwnews.com/issue/10062",
+		"https://www.dwnews.com/zone/10000117",
+		"https://www.dwnews.com/zone/10000118",
+		"https://www.dwnews.com/zone/10000119",
+		"https://www.dwnews.com/zone/10000120",
+		"https://www.dwnews.com/zone/10000123",
+		"https://www.voachinese.com",
+		"https://www.voachinese.com/z/1739",
+		"https://www.bbc.com/zhongwen/simp/topics/ck2l9z0em07t",
+		"https://chinese.aljazeera.net/news",
+		"https://cn.reuters.com/assets/jsonWireNews?limit=100",
+	}
+	for {
+		fetcher.BreadthFirst(fetcher.Crawl, worklist)
+		log.Println("Sleep 5 minutes...")
+		time.Sleep(5 * time.Minute)
+	}
+}
